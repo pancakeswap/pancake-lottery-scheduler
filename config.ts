5ca053c2580ad2ea432c8b94269553b6c7a2737f
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-06-18 15:52:13 +0200

    feat: Ticket price precision

--- config.ts
+++ config.ts
@@ -3,9 +3,15 @@ export default {
     mainnet: "0x0000000000000000000000000000000000000000",
     testnet: "0x61eB006004Ed122087036C827065aF0111d06C1A",
   },
-  TicketPrice: {
-    mainnet: 5,
-    testnet: 1,
+  Ticket: {
+    Price: {
+      mainnet: 5,
+      testnet: 0.1,
+    },
+    Precision: {
+      mainnet: 2,
+      testnet: 6,
+    },
   },
   Discount: {
     mainnet: 2000,
