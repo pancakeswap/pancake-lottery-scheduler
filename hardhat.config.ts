177f4ec3da136487969fa4ce0b641d60e33790fa
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-07-01 10:43:33 +0200

    feat: Multiple address support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,13 @@
 import type { HardhatUserConfig } from "hardhat/types";
+import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
+
+task("accounts", "Prints the list of accounts", async (args, { ethers }) => {
+  const [operator, injector] = await ethers.getSigners();
+
+  console.log(`Operator address: ${operator.address}`);
+  console.log(`Injector address: ${injector.address}`);
+});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
@@ -8,12 +16,12 @@ const config: HardhatUserConfig = {
     testnet: {
       url: "https://data-seed-prebsc-2-s2.binance.org:8545/",
       chainId: 97,
-      accounts: [process.env.PRIVATE_KEY!],
+      accounts: [process.env.OPERATOR_PRIVATE_KEY!, process.env.INJECTOR_PRIVATE_KEY!],
     },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
-      accounts: [process.env.PRIVATE_KEY!],
+      accounts: [process.env.OPERATOR_PRIVATE_KEY!, process.env.INJECTOR_PRIVATE_KEY!],
     },
   },
 };
--- scripts/draw-lottery.ts
+++ scripts/draw-lottery.ts
@@ -12,10 +12,13 @@ const main = async () => {
   // Get network data from Hardhat config (see hardhat.config.ts).
   const networkName = network.name;
 
+  // Get signers to sign the transaction(s).
+  const [operator] = await ethers.getSigners();
+
   // Check if the network is supported.
   if (networkName === "testnet" || networkName === "mainnet") {
     // Check if the private key is set (see ethers.js signer).
-    if (!process.env.PRIVATE_KEY) {
+    if (!process.env.OPERATOR_PRIVATE_KEY) {
       throw new Error("Missing private key (signer).");
     }
     // Check if the PancakeSwap Lottery smart contract address is set.
@@ -38,6 +41,7 @@ const main = async () => {
       // Create, sign and broadcast transaction.
       const tx = await contract.drawFinalNumberAndMakeLotteryClaimable(_lotteryId.toString(), true, {
         gasPrice: gasPrice.toString(),
+        from: operator,
       });
 
       const message = `[${new Date().toISOString()}] network=${networkName} block=${_blockNumber.toString()} message='Drawed lottery #${_lotteryId}' hash=${
